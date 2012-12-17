a072104cd6abf12a78297f753ba4a97bd12a7bfb
jaggyjags <jags100@hotmail.com>
2012-12-17 19:06:03 +0100

    added OC/UV

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -88,10 +88,17 @@ static struct clock_state drv_state = { 0 };
 static struct clkctl_acpu_speed *backup_s;
 
 static struct pll pll2_tbl[] = {
-	{  42, 0, 1, 0 }, /*  806 MHz */
-	{  53, 1, 3, 0 }, /* 1024 MHz */
-	{ 125, 0, 1, 1 }, /* 1200 MHz */
-	{  73, 0, 1, 0 }, /* 1401 MHz */
+	{42, 0, 1, 0 }, 	/*  806 MHz */
+	{53, 1, 3, 0 }, 	/* 1024 MHz */
+  	{58, 1, 3, 0 },     /* 1113 MHz */
+/*	{125,0, 1, 1 }, 	/  1200 MHz */
+	{63, 1, 3, 0 },     /* 1209 MHz */
+  	{68, 1, 3, 0 },     /* 1305 MHz */
+	{73, 0, 1, 0 }, 	/* 1401 MHz */
+	{78, 1, 3, 0 },		/* 1516 MHz */
+	{83, 1, 3, 0 },		/* 1612 MHz */
+	{88, 1, 3, 0 },		/* 1708 MHz */
+	{93, 1, 3, 0 },		/* 1804 MHz */
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
@@ -108,32 +115,43 @@ enum acpuclk_source {
 
 static struct clk *acpuclk_sources[MAX_SOURCE];
 
+#define SRC_LPXO (-2)
+#define SRC_AXI  (-1)
 /*
  * Each ACPU frequency has a certain minimum MSMC1 voltage requirement
  * that is implicitly met by voting for a specific minimum AXI frequency.
  * Do NOT change the AXI frequency unless you are _absoulutely_ sure you
  * know all the h/w requirements.
  */
+
+
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
-	{ 0, 24576,  LPXO, 0, 0,  30720000,  900, VDD_RAW(900) },
-	{ 0, 61440,  PLL_3,    5, 11, 61440000,  900, VDD_RAW(900) },
-	{ 1, 122880, PLL_3,    5, 5,  61440000,  900, VDD_RAW(900) },
-	{ 0, 184320, PLL_3,    5, 4,  61440000,  900, VDD_RAW(900) },
-	{ 0, MAX_AXI_KHZ, AXI, 1, 0, 61440000, 900, VDD_RAW(900) },
-	{ 1, 245760, PLL_3,    5, 2,  61440000,  900, VDD_RAW(900) },
-	{ 1, 368640, PLL_3,    5, 1,  122800000, 900, VDD_RAW(900) },
+	{ 0, 24576,  SRC_LPXO, 0, 0,  30720000,  800, VDD_RAW(800) },
+	{ 0, 61440,  PLL_3,    5, 11, 61440000,  800, VDD_RAW(800) },
+	{ 0, 122880, PLL_3,    5, 5,  61440000,  800, VDD_RAW(800) },
+	{ 0, 184320, PLL_3,    5, 4,  61440000,  800, VDD_RAW(800) },
+	{ 1, MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440000, 800, VDD_RAW(800) },
+	{ 1, 245760, PLL_3,    5, 2,  61440000,  800, VDD_RAW(800) },
+	{ 1, 368640, PLL_3,    5, 1,  122800000, 850, VDD_RAW(850) },
 	/* AXI has MSMC1 implications. See above. */
-	{ 1, 768000, PLL_1,    2, 0,  153600000, 1050, VDD_RAW(1050) },
+	{ 1, 768000, PLL_1,    2, 0,  153600000, 1000, VDD_RAW(1000) },
 	/*
 	 * AXI has MSMC1 implications. See above.
 	 */
-	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[0]},
-	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[1]},
-	{ 1, 1200000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[2]},
-	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[3]},
+	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1025, VDD_RAW(1025), &pll2_tbl[0]},
+	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[1]},
+	{ 1, 1113600, PLL_2, 3, 0, UINT_MAX, 1125, VDD_RAW(1125), &pll2_tbl[2]},
+	{ 1, 1209600, PLL_2, 3, 0, UINT_MAX, 1150, VDD_RAW(1150), &pll2_tbl[3]},
+	{ 1, 1305600, PLL_2, 3, 0, UINT_MAX, 1175, VDD_RAW(1175), &pll2_tbl[4]},
+	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[5]},
+	{ 1, 1516800, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[6]},
+	{ 1, 1612800, PLL_2, 3, 0, UINT_MAX, 1275, VDD_RAW(1275), &pll2_tbl[7]},
+	{ 1, 1708800, PLL_2, 3, 0, UINT_MAX, 1300, VDD_RAW(1300), &pll2_tbl[8]},
+	{ 1, 1804800, PLL_2, 3, 0, UINT_MAX, 1325, VDD_RAW(1325), &pll2_tbl[9]},
 	{ 0 }
 };
 
+/* Function for Ancora devices */
 #define MAX_CLK 1401600
 unsigned long acpuclk_usr_set_max(void)
 {
@@ -327,6 +345,14 @@ static unsigned long acpuclk_7x30_get_rate(int cpu)
 		return 0;
 }
 
+
+unsigned long clk_get_max_axi_khz(void)
+{
+	return MAX_AXI_KHZ;
+}
+EXPORT_SYMBOL(clk_get_max_axi_khz);
+
+
 /*----------------------------------------------------------------------------
  * Clock driver initialization
  *---------------------------------------------------------------------------*/
@@ -450,20 +476,13 @@ static inline void setup_cpufreq_table(void) { }
 void __init pll2_fixup(void)
 {
 	struct clkctl_acpu_speed *speed = acpu_freq_tbl;
-	u8 pll2_l = readl_relaxed(PLL2_L_VAL_ADDR) & 0xFF;
 
 	for ( ; speed->acpu_clk_khz; speed++) {
 		if (speed->src != PLL_2)
 			backup_s = speed;
-		if (speed->pll_rate && speed->pll_rate->l == pll2_l) {
-			speed++;
-			speed->acpu_clk_khz = 0;
-			return;
-		}
 	}
 
-	pr_err("Unknown PLL2 lval %d\n", pll2_l);
-	BUG();
+	//pr_err("Unknown PLL2 lval %d\n", pll2_l);	
 }
 
 #define RPM_BYPASS_MASK	(1 << 3)
