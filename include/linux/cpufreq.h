62cb1145589e9033bf9a120c35d9d0a3cd2af200
CastagnaIT <whiteflash@email.it>
2013-01-05 14:38:17 +0100

    cpufreq: added fantasy governor (in test)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,6 +375,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lulzactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_FANTASY)
+extern struct cpufreq_governor cpufreq_gov_fantasy;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_fantasy)
 #endif
 
 
