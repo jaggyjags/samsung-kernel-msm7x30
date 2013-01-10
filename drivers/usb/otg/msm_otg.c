dfdf0dad3833d259ef8c22288419115f4db86203
jaggyjags <jags100@hotmail.com>
2013-01-10 23:28:28 +0100

    attempt to add fast charge support, usb is not detected as AC..

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -44,6 +44,14 @@
 #include <mach/msm_xo.h>
 #include <mach/msm_bus.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif
+
+bool fast_charge = false;
+module_param(fast_charge, bool, 0775);
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -934,6 +942,17 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+			mA = USB_FASTCHG_LOAD;
+			pr_info("USB fast charging is ON - 1000mA.\n");
+	} else {
+		pr_info("USB fast charging is OFF.\n");
+	}
+#endif
+
+	if (fast_charge)
+		mA = 1000
 
 	dev_info(motg->otg.dev, "Avail curr from USB = %u\n", mA);
 
